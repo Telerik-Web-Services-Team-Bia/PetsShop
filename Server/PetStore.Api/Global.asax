1a212c42b9ab11e3b610b67344601e461ff5cffd
Nikolai Novkirishki <novkirishki94@gmail.com>
2015-11-08 10:11:56 +0200

    Added web.Api base
    Cleaned all unneeded stuff to provide only restful services

---
+++ Server/PetStore.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetStore.Api.WebApiApplication" Language="C#" %>
---
+++ Server/PetStore.Api/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿namespace PetStore.Api
+{
+    using System.Web;
+    using System.Web.Http;
+
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
