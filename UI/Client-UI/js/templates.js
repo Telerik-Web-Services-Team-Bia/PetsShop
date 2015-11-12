c5503bc9eacd0a454812bb753d7b9c9359774736
Ivaylo Andonov <ivailo.kolev.andonov@gmail.com>
2015-11-12 17:19:39 +0200

    Refactored the UI part, server added
    AjaxRequester, dataTemplates, new rauts added.
    Geting started to write the requests as clean as possible from client to
    the Web service.

---
+++ UI/Client-UI/js/templates.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Ивайло on 12.11.2015 г..
+ */
+var templates = function() {
+    var handlebars = window.handlebars || window.Handlebars,
+        cache = {};
+
+    function get(name) {
+        var promise = new Promise(function(resolve, reject) {
+            if (cache[name]) {
+                resolve(cache[name]);
+                return;
+            }
+            var url = "views/" + name + ".handlebars.html";
+            $.get(url, function(html) {
+                var template = handlebars.compile(html);
+                cache[name] = template;
+                resolve(template);
+            });
+        });
+        return promise;
+    }
+    return {
+        get: get
+    };
+}();
