6162135ab57dfbbc7a8bbb8a547d86b246dcbb2c
DennyGD <denny.dimitrova@abv.bg>
2015-11-15 13:36:04 +0200

    Added mocks factory

---
+++ Tests/PetsShop.Tests/Mocks/MocksFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace PetsShop.Tests.Mocks
+{
+    using PetStore.Models;
+
+    public static class MocksFactory
+    {
+        public static RepositoryMock<Category> GetCategoriesRepository(int categoriesCount = 5)
+        {
+            var repo = new RepositoryMock<Category>();
+
+            for (int i = 0; i < categoriesCount; i++)
+            {
+                repo.Add(new Category
+                {
+                    Name = "Test category " + i
+                });
+            }
+
+            return repo;
+        }
+    }
+}
