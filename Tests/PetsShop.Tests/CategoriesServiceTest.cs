4d5476980b82d18010af6516932fdd264522710e
DennyGD <denny.dimitrova@abv.bg>
2015-11-15 13:48:04 +0200

    Started testing CategoriesService; 4.5.2
    Changed Tests project framework to 4.5.2

---
+++ Tests/PetsShop.Tests/CategoriesServiceTest.cs
@@ -0,0 +1,32 @@
+﻿namespace PetsShop.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Mocks;
+    using PetStore.Services.Data.Contracts;
+    using PetStore.Models;
+    using PetStore.Services.Data;
+
+    [TestClass]
+    public class CategoriesServiceTest
+    {
+        private ICategoriesService categoriesService;
+
+        private RepositoryMock<Category> categoriesRepo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.categoriesRepo = MocksFactory.GetCategoriesRepository();
+            this.categoriesService = new CategoriesService(this.categoriesRepo);
+        }
+
+        [TestMethod]
+        public void AddShouldInvokeSaveChanges()
+        {
+            var result = this.categoriesService.Add("Test name");
+            Assert.AreEqual(1, this.categoriesRepo.SavesCount);
+        }
+    }
+}
